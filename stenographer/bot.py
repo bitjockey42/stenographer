3875fca2a12b7e62e95dfa14ecac899bc036fbbd
bitjockey <bitjockey@jackpoint.network>
2024-09-21 07:29:14 -0400

    Extract into method

--- stenographer/bot.py
+++ stenographer/bot.py
@@ -43,18 +43,6 @@ async def on_ready():
 async def export(ctx):
     await ctx.channel.send(f"Hello {ctx.author.display_name}")
 
-    channel = bot.get_channel(ctx.channel.id)
-
-    from_date = None
-    async for message in channel.history(limit=1, oldest_first=True):
-        from_date = message.created_at - timedelta(days=1)
-
-    to_date = None
-    async for message in channel.history(limit=1):
-        to_date = message.created_at
-
-    print(from_date, to_date)
-
     with open("test.csv", "w+", newline="") as csvfile:
         writer = csv.DictWriter(
             csvfile,
@@ -64,31 +52,38 @@ async def export(ctx):
         )
         writer.writeheader()
 
-        while from_date <= to_date:
-            async for message in channel.history(
-                after=from_date, limit=100, oldest_first=True
-            ):
-                # if bool(message.thread):
-                #     thread = bot.get_channel(message.thread.id)
-                #     from_thread_date
+        await write_message_history(ctx.channel.id, writer)
 
-                row = {
-                    "created_at": message.created_at,
-                    "clean_content": message.clean_content,
-                    "author": message.author.display_name,
-                    "author_id": message.author.id,
-                    "thread": None,
-                    "thread_id": None,
-                }
 
-                writer.writerow(row)
+async def write_message_history(channel_or_thread_id, writer):
+    channel = bot.get_channel(channel_or_thread_id)
+    from_date = None
+    async for message in channel.history(limit=1, oldest_first=True):
+        from_date = message.created_at - timedelta(days=1)
 
-                from_date = message.created_at
+    to_date = None
+    async for message in channel.history(limit=1):
+        to_date = message.created_at
 
-            if from_date == to_date:
-                break
+    while from_date <= to_date:
+        async for message in channel.history(
+            after=from_date, limit=100, oldest_first=True
+        ):
+            row = {
+                "created_at": message.created_at,
+                "clean_content": message.clean_content,
+                "author": message.author.display_name,
+                "author_id": message.author.id,
+                "thread": None,
+                "thread_id": None,
+            }
 
-    # await ctx.channel.send("Exported")
+            writer.writerow(row)
+
+            from_date = message.created_at
+
+        if from_date == to_date:
+            break
 
 
 bot.run(DISCORD_BOT_TOKEN)
